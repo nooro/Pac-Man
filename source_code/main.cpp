869b20c26ec2e74259c6e3adb8a849eef091881e
Dancheff <georgi.dancheff@gmail.com>
2017-01-13 01:00:22 +0200

    Made initialization for the game scene and run it

--- source_code/main.cpp
+++ source_code/main.cpp
@@ -1,11 +1,9 @@
 #include "System/system.h"
-#include "Entities/pac_man.h"
 #include "Scenes/game.h"
 
 int main(int argc, char ** argv)
 {
     if( !System::Init() ) { return -1; }
-    if( !PacMan::Init() ) { return -1; }
     if( !GameScene::Init() ) { return -1; }
 
     GameScene::Play();
