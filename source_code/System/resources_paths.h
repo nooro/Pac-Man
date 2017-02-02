2e2b0e5d29982218ee4f515d3c0ac7bab5f1e598
Dancheff <georgi.dancheff@gmail.com>
2017-02-02 14:47:18 +0200

    Added paths to ghosts' idle image and walk animation

--- source_code/System/resources_paths.h
+++ source_code/System/resources_paths.h
@@ -5,7 +5,9 @@
 #define PAC_MAN_WALK_ANIMATION  "Resources/Animations/PacMan/walk.anim"
 #define PAC_MAN_DIE_ANIMATION   "Resources/Animations/PacMan/die.anim"
 
-#define POINT_IMAGE "Resources/Images/point.png"
-#define WALL_IMAGE  "Resources/Images/wall.png"
+#define POINT_IMAGE             "Resources/Images/point.png"
+#define WALL_IMAGE              "Resources/Images/wall.png"
 
+#define GHOST_IDLE_IMAGE        "Resources/Images/ghost.png"
+#define GHOST_WALK_ANIMATION    "Resources/Animations/Ghost/walk.anim"
 #endif // RESOURCES_PATHS_H_INCLUDED
