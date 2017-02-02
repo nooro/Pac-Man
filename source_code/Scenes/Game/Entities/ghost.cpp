0c524b92f541627c4cecbf8b2deab05cb293bfe2
Dancheff <georgi.dancheff@gmail.com>
2017-02-02 15:37:14 +0200

    Started the development of ghosts' basic methods

--- source_code/Scenes/Game/Entities/ghost.cpp
+++ source_code/Scenes/Game/Entities/ghost.cpp
@@ -1,1 +1,82 @@
 #include "ghost.h"
+
+bool Ghost::Init(int hight, int width)
+{
+    this->is_alive = true;
+    this->is_walking = true;
+
+    this->rect.w = width;
+    this->rect.h = hight;
+    this->movement_speed = this->rect.w / 6;
+
+    this->idle_image.SetTexture( GHOST_IDLE_IMAGE );
+    if( this->idle_image.texture == NULL ) { return false; }
+
+    this->walk_animation.Create( GHOST_WALK_ANIMATION, 3, movement_speed * 3 , AnimationType::GoBack );
+    if( this->walk_animation.texture == NULL ) { return false; }
+
+    this->walk_animation.rect = this->rect;
+    this->idle_image.rect = this->rect;
+
+    this->walk_direction = Direction::Right;
+
+    return true;
+}
+
+void Ghost::Update()
+{
+    this->Move();
+
+    this->walk_animation.rect.x = this->rect.x;
+    this->walk_animation.rect.y = this->rect.y;
+}
+
+void Ghost::Move()
+{
+    if( this->is_walking )
+    {
+        if( this->walk_direction == this->Direction::Right )
+            { this->rect.x += this->movement_speed; }
+
+        else if( this->walk_direction == this->Direction::Left )
+            { this->rect.x -= this->movement_speed; }
+
+        else if( this->walk_direction == this->Direction::Up )
+            { this->rect.y -= this->movement_speed; }
+
+        else if( this->walk_direction == this->Direction::Down )
+            { this->rect.y += this->movement_speed; }
+    }
+}
+
+void Ghost::Die()
+{
+
+}
+
+void Ghost::Render()
+{
+    if(this->is_walking)
+    {
+        this->walk_animation.PlayNextFrame();
+    }
+    else
+    {
+        this->idle_image.Render();
+    }
+}
+
+int Ghost::GetX() { return this->rect.x; }
+int Ghost::GetY() { return this->rect.y; }
+int Ghost::GetWidth() { return this->rect.w; }
+int Ghost::GetHeight() { return this->rect.h; }
+
+void Ghost::SetMovementSpeed(int speed)
+{
+    this->movement_speed = speed;
+}
+
+int Ghost::GetMovementSpeed()
+{
+    return this->movement_speed;
+}
--- source_code/Scenes/Game/Entities/ghost.h
+++ source_code/Scenes/Game/Entities/ghost.h
@@ -1,12 +1,40 @@
 #ifndef GHOST_H_INCLUDED
 #define GHOST_H_INCLUDED
 
+#include "../../../System/system.h"
+#include "../../../GO/go.h"
+
 class Ghost
 {
     public:
+        bool Init(int high, int width);
+        void Render();
+
+        void Update();
+
+        enum Direction { Right, Left, Up = 90, Down = 270 };
+        Direction walk_direction;
+
+        void Die();
+
+        int GetX();
+        int GetY();
+        int GetWidth();
+        int GetHeight();
+
+        void SetMovementSpeed(int speed);
+        int GetMovementSpeed();
 
     private:
+        void Move();
 
+        SDL_Rect rect;
+
+        bool is_alive, is_walking;
+        int movement_speed;
+
+        Image idle_image;
+        Animation walk_animation;
 };
 
 #endif // GHOST_H_INCLUDED
