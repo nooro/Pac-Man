b8561415f2fdb58c0c258d5265c77f3220cd0d70
Dancheff <georgi.dancheff@gmail.com>
2017-02-07 00:49:10 +0200

    Added get_coin sound path

--- source_code/System/resources_paths.h
+++ source_code/System/resources_paths.h
@@ -1,16 +1,23 @@
 #ifndef RESOURCES_PATHS_H_INCLUDED
 #define RESOURCES_PATHS_H_INCLUDED
 
+//Pac-Man images and animations
 #define PAC_MAN_IDLE_IMAGE      "Resources/Images/pac_man.png"
 #define PAC_MAN_WALK_ANIMATION  "Resources/Animations/PacMan/walk.anim"
 #define PAC_MAN_DIE_ANIMATION   "Resources/Animations/PacMan/die.anim"
 
+//Map images
 #define POINT_IMAGE             "Resources/Images/point.png"
 #define WALL_IMAGE              "Resources/Images/wall.png"
 
+//Ghosts images and animations
 #define GHOST_IDLE_IMAGE        "Resources/Images/ghost.png"
 #define GHOST_WALK_ANIMATION    "Resources/Animations/Ghost/walk.anim"
 
-#define FONT_ITC            "Resources/Fonts/itc.ttf"
+//Fonts
+#define FONT_ITC                "Resources/Fonts/itc.ttf"
+
+//Sounds
+#define SOUND_GET_COIN          "Resources/Sounds/get_coin.wav"
 
 #endif // RESOURCES_PATHS_H_INCLUDED
